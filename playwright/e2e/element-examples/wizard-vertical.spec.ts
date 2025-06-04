5d6b9ea987b8d3dae7c12c48be5b6a7bbd771339
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-06-04 14:12:28 +0200

    test(wizard): introduce e2e tests
    Provide e2e tests to prevent unattended visual changes

---
+++ playwright/e2e/element-examples/wizard-vertical.spec.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright Siemens 2016 - 2025.
+ * SPDX-License-Identifier: MIT
+ */
+import { expect, test } from '../../support/test-helpers';
+
+test.describe('si-wizard-vertical', () => {
+  const example = 'si-wizard/si-wizard-playground';
+
+  test(example, async ({ page, si }) => {
+    await si.visitExample(example);
+    await page.getByLabel('Vertical layout', { exact: true }).check();
+    await si.runVisualAndA11yTests();
+
+    await page.getByLabel('Show vertical divider').check();
+    await si.runVisualAndA11yTests('vertical-divider');
+    await page.getByLabel('Show vertical divider').uncheck();
+
+    await page.getByLabel('Vertical layout', { exact: true }).uncheck();
+    await si.runVisualAndA11yTests('horizontal');
+  });
+
+  test(example + ' on tab', async ({ page, si }) => {
+    await si.visitExample(example);
+    await page.getByLabel('Vertical layout', { exact: true }).check();
+    // note: click in the top-left corner, otherwise the step gets the focus
+    await (await page.locator('si-wizard')).click({ position: { x: 10, y: 10 } });
+    await page.keyboard.press('Tab');
+    const step = await page.locator('a:focus-visible');
+    expect(step).toContainText('Step 2');
+
+    await si.runVisualAndA11yTests('focus');
+  });
+
+  test(example + ' step numbers', async ({ page, si }) => {
+    await si.visitExample(example);
+    await page.getByLabel('Vertical layout', { exact: true }).check();
+    await page.getByLabel('Number representation').check();
+    await page.getByLabel('Number of steps', { exact: true }).fill('13');
+    for (let i = 0; i < 10; i++) {
+      await page.locator('si-wizard').getByLabel('Next').click();
+    }
+
+    await si.runVisualAndA11yTests('step-numbers');
+  });
+});
---
+++ src/app/examples/si-wizard/si-wizard-playground.ts
@@ -0,0 +1,71 @@
+/**
+ * Copyright Siemens 2016 - 2025.
+ * SPDX-License-Identifier: MIT
+ */
+import { NgClass } from '@angular/common';
+import { Component, inject, OnInit, signal, viewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { SiFormItemComponent } from '@siemens/element-ng/form';
+import { SiNumberInputComponent } from '@siemens/element-ng/number-input';
+import { SiWizardComponent, SiWizardStepComponent } from '@siemens/element-ng/wizard';
+import { LOG_EVENT } from '@siemens/live-preview';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './si-wizard-playground.html',
+  imports: [
+    ReactiveFormsModule,
+    SiNumberInputComponent,
+    SiFormItemComponent,
+    SiWizardComponent,
+    SiWizardStepComponent,
+    NgClass
+  ]
+})
+export class SampleComponent implements OnInit {
+  protected readonly logEvent = inject(LOG_EVENT);
+  protected readonly wizard = viewChild.required(SiWizardComponent);
+  protected readonly configForm = new FormGroup({
+    enableCompletionPage: new FormControl<boolean>(true, { nonNullable: true }),
+    completionPageVisibleTime: new FormControl<number>(2000, { nonNullable: true }),
+    validStep: new FormControl<boolean>(true, { nonNullable: true }),
+    hasCancel: new FormControl<boolean>(false, { nonNullable: true }),
+    hasNavigation: new FormControl<boolean>(true, { nonNullable: true }),
+    verticalLayout: new FormControl(false),
+    inlineNavigation: new FormControl(true),
+    cardWizard: new FormControl(true),
+    showVerticalDivider: new FormControl(false),
+    showStepNumbers: new FormControl(false),
+    stepCount: new FormControl<number>(3, { nonNullable: true }),
+    verticalLayoutMinSize: new FormControl<number | undefined>(undefined),
+    verticalLayoutMaxSize: new FormControl<number | undefined>(undefined)
+  });
+
+  protected readonly steps = signal<string[]>([]);
+
+  ngOnInit(): void {
+    this.generateSteps(this.configForm.value.stepCount!);
+    this.configForm.controls.verticalLayout.valueChanges.subscribe(v => {
+      if (v) {
+        // vertical layout implies inline navigation
+        this.configForm.controls.inlineNavigation.setValue(false);
+      } else {
+        this.configForm.controls.showVerticalDivider.setValue(false);
+      }
+    });
+    this.configForm.valueChanges.subscribe(v => {
+      if (v.stepCount && this.steps().length !== v.stepCount) {
+        this.generateSteps(v.stepCount);
+      }
+    });
+  }
+
+  protected canNext(): boolean {
+    const wizard = this.wizard();
+    return wizard.index + 1 < wizard.stepCount && !!wizard.currentStep?.isNextNavigable;
+  }
+
+  private generateSteps(amount: number): void {
+    this.steps.set(new Array(amount).fill(1).map((value, index) => `Step ${index + 1}`));
+  }
+}
