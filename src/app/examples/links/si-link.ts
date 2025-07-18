11361308ec4cd1f42de3c1acc4d401d29cc1a85e
Maximilian Koeller <maximilian.koeller@siemens.com>
2025-07-18 11:35:22 +0200

    docs(link): add missing example

---
+++ src/app/examples/links/si-link.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Link, SiLinkModule } from '@siemens/element-ng/link';
+
+@Component({
+  selector: 'app-sample',
+  imports: [SiLinkModule],
+  templateUrl: './si-link.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'p-5' }
+})
+export class SampleComponent {
+  simplHrefLink: Link = { href: 'https://simpl.siemens.io', tooltip: 'APP.CLAIM' };
+  simplActionLink: Link = { action: () => alert('Hello SiMPL!'), tooltip: 'APP.CLAIM' };
+  activeRouterLink: Link = { link: '.', tooltip: 'APP.CLAIM' };
+  otherRouterLink: Link = { link: '../links', tooltip: 'APP.CLAIM' };
+}
