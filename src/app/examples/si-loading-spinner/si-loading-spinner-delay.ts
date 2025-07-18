cd8bb07da7876a8b3025d18693193e2ec8fbb5fe
Maximilian Koeller <maximilian.koeller@siemens.com>
2025-07-18 09:32:21 +0200

    docs(loading-spinner): add missing example

---
+++ src/app/examples/si-loading-spinner/si-loading-spinner-delay.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { SiCardComponent } from '@siemens/element-ng/card';
+import { SiLoadingService, SiLoadingSpinnerDirective } from '@siemens/element-ng/loading-spinner';
+
+@Component({
+  selector: 'app-sample',
+  imports: [CommonModule, SiCardComponent, SiLoadingSpinnerDirective],
+  templateUrl: './si-loading-spinner-delay.html'
+})
+export class SampleComponent {
+  protected loadingService = inject(SiLoadingService);
+  initialDelay = true;
+  blockingSpinner = true;
+
+  start(): void {
+    this.loadingService.startLoad();
+  }
+
+  stop(): void {
+    this.loadingService.stopLoad();
+  }
+
+  delay(): void {
+    this.initialDelay = !this.initialDelay;
+  }
+
+  blocking(): void {
+    this.blockingSpinner = !this.blockingSpinner;
+  }
+}
