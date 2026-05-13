f9b9903ddd71c6310466ae6314f7b147d17b9fc4
Bajohr, Rayk <rayk.bajohr@siemens.com>
2026-05-13 06:59:18 +0200

    refactor(menu): migrate to component bindings

--- projects/element-ng/menu/si-menu-bar.directive.ts
+++ projects/element-ng/menu/si-menu-bar.directive.ts
@@ -3,21 +3,21 @@
  * SPDX-License-Identifier: MIT
  */
 import { CdkMenuBar, CdkTargetMenuAim } from '@angular/cdk/menu';
-import { Directive, HostBinding, inject, input } from '@angular/core';
+import { Directive, inject, input } from '@angular/core';
 
 @Directive({
   // eslint-disable-next-line @angular-eslint/directive-selector
   selector: 'si-menu-bar',
   host: {
     class: 'd-inline-flex',
-    style: 'gap: 1px'
+    style: 'gap: 1px',
+    '[tabindex]': 'tabIndex'
   },
   hostDirectives: [CdkMenuBar, CdkTargetMenuAim]
 })
 export class SiMenuBarDirective {
   private menuBar = inject(CdkMenuBar, { self: true });
 
-  @HostBinding('tabindex')
   protected get tabIndex(): 0 | -1 | null {
     return this.disabled() ? -1 : this.menuBar._getTabIndex();
   }
