b937e9e683a300db9de0d1d25bd8c4079544ab09
chintankavathia <chintankumar.kavathia@siemens.com>
2026-05-13 18:04:30 +0530

    test(e2e): resolve flaky tour tests

--- playwright/e2e/element-examples/si-tour.spec.ts
+++ playwright/e2e/element-examples/si-tour.spec.ts
@@ -11,22 +11,29 @@ test.describe('si-tour', () => {
     const nextClick = (): Promise<any> =>
       page.locator('si-tour button', { hasText: 'Next' }).click();
 
+    const waitForTourStep = async (hasArrow = true): Promise<void> => {
+      await expect(page.locator('si-tour .tour-content.show')).toBeVisible();
+      if (hasArrow) {
+        await expect(page.locator('si-tour .popover-arrow')).toBeVisible();
+      }
+    };
+
     await si.visitExample(example);
-    await expect(page.locator('.tour-content')).toBeVisible();
+    await waitForTourStep();
     await si.runVisualAndA11yTests('first-step');
 
     await nextClick();
-    await expect(page.locator('.tour-content')).toBeVisible();
+    await waitForTourStep();
     await si.runVisualAndA11yTests('second-step');
 
     await nextClick();
     await nextClick();
     await nextClick();
     await nextClick();
-    await expect(page.locator('.tour-content')).toBeVisible();
+    await waitForTourStep();
     await si.runVisualAndA11yTests('open-menu');
     await nextClick();
-    await expect(page.locator('.tour-content')).toBeVisible();
+    await waitForTourStep(false);
     await si.runVisualAndA11yTests('last-step');
   });
 });
